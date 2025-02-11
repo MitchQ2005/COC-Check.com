410342c3a19844712c68bc4c7b35a349b2c2b597
Julian-Forrer <julianforrer@hotmail.nl>
2025-02-11 14:02:43 +0100

    route aangemaakt voor controller not final

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BaseController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Route voor de CRUD vanuit de Base controller
+Route::resource('bases', BaseController::class);
 
 require __DIR__.'/auth.php';

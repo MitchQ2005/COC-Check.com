10f80cc0f1beee07d59ea4479ea65d4cf5528d88
Julian-Forrer <julianforrer@hotmail.nl>
2025-02-21 12:52:30 +0100

    login voor home pagina gemaakt en eigen layout ingeladen voor login stappen, nu nog eigen design maken

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,8 +21,32 @@
                     <li class="bg-orange-500 rounded-lg border border-white">
                         <a href="{{ route('home') }}" class="text-white px-4 py-2 block">Contact</a>
                     </li>
+                    <li class="relative bg-[#DB5A42] rounded-lg border border-white group">
+                        <a href="{{ route('login') }}" class="text-white px-4 py-2 block">Login</a>
+                        <ul class="absolute hidden text-gray-700 pt-1 group-hover:block z-50">
+                            @auth
+                                <li class="">
+                                    <a href="{{ route('dashboard') }}" class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Dashboard</a>
+                                </li>
+                                <li class="">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit" class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Logout</button>
+                                    </form>
+                                </li>
+                            @else
+                                <li class="">
+                                    <a href="{{ route('register') }}" class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Register</a>
+                                </li>
+                            @endauth
+                        </ul>
+                    </li>
                     <li class="bg-[#DB5A42] rounded-lg border border-white">
-                        <a href="{{ route('home') }}" class="text-white px-4 py-2 block">Login</a>
+                        @auth
+                            <span class="text-white px-4 py-2 block">Logged in as {{ Auth::user()->name }}</span>
+                        @else
+                            <span class="text-white px-4 py-2 block">Currently not logged in</span>
+                        @endauth
                     </li>
                 </ul>
             </nav>

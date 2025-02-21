80fe41ee710508b70f6c773510522e9375dd8a70
Julian-Forrer <julianforrer@hotmail.nl>
2025-02-21 14:37:25 +0100

    styling the dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h2>{{ __("welcome " . Auth::user()->name . "!") }}</h2>
+                    <br>
+                    this is your user dashboard where you can upload, and manage your existing base layouts. you can also manage your account from here.
+                </div>
+            </div>
+            <div class="mt-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg relative h-64">
+                    <div class="absolute inset-0 bg-cover bg-center opacity-50" style="background-image: url('{{ asset('images/coc-check bases.png') }}');"></div>
+                    <div class="relative p-6 text-gray-900 flex items-center justify-center h-full">
+                        <a href="{{ route('bases.create') }}" class="text-blue-500 hover:underline">Upload Base</a>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg relative h-64">
+                    <div class="absolute inset-0 bg-cover bg-center opacity-50" style="background-image: url('{{ asset('images/my-bases.jpg') }}')"></div>
+                    <div class="relative p-6 text-gray-900 flex items-center justify-center h-full">
+                        <a href="{{ route('bases.index') }}" class="text-blue-500 hover:underline">My Bases</a>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg relative h-64">
+                    <div class="absolute inset-0 bg-cover bg-center opacity-50" style="background-image: url('{{ asset('images/edit profile.png') }}');"></div>
+                    <div class="relative p-6 text-gray-900 flex items-center justify-center h-full">
+                        <a href="{{ route('profile.edit') }}" class="text-blue-500 hover:underline">Edit Profile</a>
+                    </div>
                 </div>
             </div>
         </div>
